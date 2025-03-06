de7d1436d46f26579a189ec078712ca40014e653
phapvuong <btphap.91@gmail.com>
2025-03-06 22:45:30 +0700

    cập nhật

--- Ex6_IntentDonGian/app/src/main/java/ntu/buithanhphap/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/ntu/buithanhphap/ex6_intentdongian/MainActivity.java
@@ -1,5 +1,6 @@
 package ntu.buithanhphap.ex6_intentdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Tìm điều khiển nút bấm
         TimDieuKhien();
-        //
+        // Gắn bộ lắng nghe sự kiện
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo một Intent 2 tham số: 1) màn hình hiện tại.this; 2) màn hình chuyển tới.class
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                //B2 Gửi
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo một Intent 2 tham số: 1) màn hình hiện tại.this; 2) màn hình chuyển tới.class
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                //B2 Gửi
+                startActivity(intentMH3);
+            }
+        });
     }
 }
