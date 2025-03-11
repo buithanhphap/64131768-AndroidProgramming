2e7fa18fc9d1f09dc3390fac8a23caaba2dd9649
phapvuong <btphap.91@gmail.com>
2025-03-11 23:06:56 +0700

    Đã hoàn thành xong

--- LVNgonNguLapTrinh/app/src/main/java/ntu/buithanhphap/lvngonngulaptrinh/MainActivity.java
+++ LVNgonNguLapTrinh/app/src/main/java/ntu/buithanhphap/lvngonngulaptrinh/MainActivity.java
@@ -1,8 +1,11 @@
 package ntu.buithanhphap.lvngonngulaptrinh;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,11 +27,26 @@ public class MainActivity extends AppCompatActivity {
         //B1. Chuẩn bị dữ liệu hard-code
         dsNgonNguLT = new ArrayList<String>();
         dsNgonNguLT.add("Python");
+        dsNgonNguLT.add("Php");
+        dsNgonNguLT.add("C++");
+        dsNgonNguLT.add("C#");
+        dsNgonNguLT.add("Java");
         //B2.
         ArrayAdapter<String> adapterNNLT;
         adapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsNgonNguLT);
         //B3. Gắn adapter
         listViewNNLT.setAdapter(adapterNNLT);
         //B4. Gắn bộ lắng nghe và xử lý sự kiện
+        listViewNNLT.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //code trực tiếp xử lý ở đây
+                //chú ý: biến position đã chứa vị trí của item được click
+                String giaTriDuocChon = dsNgonNguLT.get(position);
+                //làm gì đó với giá trị này thì tùy
+                //đơn giản, ta Toast lên
+                Toast.makeText(MainActivity.this, giaTriDuocChon, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
