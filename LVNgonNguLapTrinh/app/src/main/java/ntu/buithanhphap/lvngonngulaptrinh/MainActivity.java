b59d3a431c3690daeb182d0720340426ac631aff
phapvuong <btphap.91@gmail.com>
2025-03-11 22:35:21 +0700

    đã làm tới bước 3 gắn adapter

--- LVNgonNguLapTrinh/app/src/main/java/ntu/buithanhphap/lvngonngulaptrinh/MainActivity.java
+++ LVNgonNguLapTrinh/app/src/main/java/ntu/buithanhphap/lvngonngulaptrinh/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.buithanhphap.lvngonngulaptrinh;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ListView listViewNNLT;
+    ArrayList<String> dsNgonNguLT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        listViewNNLT = findViewById(R.id.listviewNNLT);
+        //B1. Chuẩn bị dữ liệu hard-code
+        dsNgonNguLT = new ArrayList<String>();
+        dsNgonNguLT.add("Python");
+        //B2.
+        ArrayAdapter<String> adapterNNLT;
+        adapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsNgonNguLT);
+        //B3. Gắn adapter
     }
 }
