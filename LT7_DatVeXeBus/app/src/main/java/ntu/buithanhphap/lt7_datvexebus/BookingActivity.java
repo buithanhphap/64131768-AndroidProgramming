084fb95cf4d0dfd1638fedff4d1313994cb137f5
phapvuong <btphap.91@gmail.com>
2025-03-15 16:42:38 +0700

    update

--- LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BookingActivity.java
+++ LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BookingActivity.java
@@ -1,6 +1,8 @@
 package ntu.buithanhphap.lt7_datvexebus;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class BookingActivity extends AppCompatActivity {
-
+    EditText edtName, edtPhoneNumber, edtTickets;
+    Button btnDatVe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
     }
+
 }
--- LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BusDetailsActivity.java
+++ LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BusDetailsActivity.java
@@ -1,6 +1,9 @@
 package ntu.buithanhphap.lt7_datvexebus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +19,18 @@ public class BusDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_details);
+        TextView tvBusName = findViewById(R.id.tvBusName);
+        Button btnBookTicket = findViewById(R.id.btnBookTicket);
+
+        // Nhận dữ liệu từ MainActivity
+        String tenChuyen = getIntent().getStringExtra("ten_chuyen");
+        tvBusName.setText(tenChuyen);
+
+        // Nút đặt vé
+        btnBookTicket.setOnClickListener(v -> {
+            Intent bookIntent = new Intent(BusDetailsActivity.this, BookingActivity.class);
+            bookIntent.putExtra("ten_chuyen", tenChuyen);
+            startActivity(bookIntent);
+        });
     }
 }
