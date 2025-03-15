161d645036f3d6e85d0674cefd923a176e87f034
phapvuong <btphap.91@gmail.com>
2025-03-15 16:22:57 +0700

    update

--- LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BusDetailsActivity.java
+++ LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BusDetailsActivity.java
@@ -7,9 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class BusDetailsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
