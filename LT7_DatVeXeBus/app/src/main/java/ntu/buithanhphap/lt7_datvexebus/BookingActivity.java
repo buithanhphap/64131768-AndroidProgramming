8af70b1ca5f47eb1413e8d710f5cedb49b4dafa9
phapvuong <btphap.91@gmail.com>
2025-03-15 16:49:24 +0700

    update

--- LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BookingActivity.java
+++ LT7_DatVeXeBus/app/src/main/java/ntu/buithanhphap/lt7_datvexebus/BookingActivity.java
@@ -1,8 +1,11 @@
 package ntu.buithanhphap.lt7_datvexebus;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,22 @@ public class BookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
-    }
+        TextView tvBusName = findViewById(R.id.tvBusName);
+        edtName = findViewById(R.id.edtName);
+        edtPhoneNumber = findViewById(R.id.edtPhoneNumber);
+        edtTickets = findViewById(R.id.edtTickets);
+        btnDatVe = findViewById(R.id.btnDatVe);
 
+        // Nhận dữ liệu từ BusDetailsActivity
+        String tenChuyen = getIntent().getStringExtra("ten_chuyen");
+        tvBusName.setText(tenChuyen);
+
+        // Nút xác nhận đặt vé
+        btnDatVe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BookingActivity.this, "Đặt vé thành công!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
