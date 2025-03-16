ff1a23a1337df99aa4d14f261e1b2ca104ca85cc
phapvuong <btphap.91@gmail.com>
2025-03-16 16:06:46 +0700

    update cần tạo thêm xml cho luật chơi

--- LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/MenuActivity.java
+++ LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/MenuActivity.java
@@ -1,6 +1,8 @@
 package ntu.buithanhphap.lt8_gamenhinhinhdoanchu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -19,5 +21,29 @@ public class MenuActivity extends AppCompatActivity {
         btnPlay = findViewById(R.id.btnPlay);
         btnRules = findViewById(R.id.btnRules);
         btnExit = findViewById(R.id.btnExit);
+
+        // Xử lý sự kiện nút Chơi
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, RulesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
