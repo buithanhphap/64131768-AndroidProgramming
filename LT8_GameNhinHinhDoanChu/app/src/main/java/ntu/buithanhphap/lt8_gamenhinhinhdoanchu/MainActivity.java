d36b8cd0b2044e0b4d3d9fe61c7dff272babc2dc
phapvuong <btphap.91@gmail.com>
2025-03-16 15:01:09 +0700

    cập nhật

--- LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/MainActivity.java
+++ LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/MainActivity.java
@@ -13,11 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     ImageView ivImage;
     TextInputEditText edtTraLoi;
     TextView tvDiem, tvDiemCaoNhat;
     Button btnKiemTra, btnTiepTheo;
+
+    String[] images = {"daoaothaca", "cauchi"};
+    int[] imageRes = {R.drawable.daoaothaca, R.drawable.cauchi};
+    String correctAnswer;
+    int score = 0;
+    HighScoreManager highScoreManager;
+    Random random = new Random();
+    int currentIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
