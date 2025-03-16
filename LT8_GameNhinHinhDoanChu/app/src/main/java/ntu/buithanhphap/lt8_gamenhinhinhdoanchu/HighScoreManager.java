fb0e8b83d6888a057d62b52de85446cb179134d9
phapvuong <btphap.91@gmail.com>
2025-03-16 15:18:29 +0700

    cập nhật

--- LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/HighScoreManager.java
+++ LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/HighScoreManager.java
@@ -1,4 +1,12 @@
 package ntu.buithanhphap.lt8_gamenhinhinhdoanchu;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class HighScoreManager {
+    SharedPreferences sharedPreferences;
+
+    public HighScoreManager(Context context) {
+        sharedPreferences = context.getSharedPreferences("GamePrefs", Context.MODE_PRIVATE);
+    }
 }
