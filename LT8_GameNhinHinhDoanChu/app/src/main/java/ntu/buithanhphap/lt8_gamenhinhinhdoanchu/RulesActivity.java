869d239a54518b10163cc3cf6743d6554a908ebb
phapvuong <btphap.91@gmail.com>
2025-03-16 16:12:47 +0700

    khai báo

--- LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/RulesActivity.java
+++ LT8_GameNhinHinhDoanChu/app/src/main/java/ntu/buithanhphap/lt8_gamenhinhinhdoanchu/RulesActivity.java
@@ -1,6 +1,7 @@
 package ntu.buithanhphap.lt8_gamenhinhinhdoanchu;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RulesActivity extends AppCompatActivity {
-
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rules);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
