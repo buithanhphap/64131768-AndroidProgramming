7cf4bf8d30aca46468756c4e24bf1af0b5281541
phapvuong <btphap.91@gmail.com>
2025-03-19 19:17:06 +0700

    update

--- BaiTrenLop/app/src/main/java/ntu/buithanhphap/baitrenlop/MainActivity.java
+++ BaiTrenLop/app/src/main/java/ntu/buithanhphap/baitrenlop/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnKiemTra;
     TextView tvSoA, tvSoB, tvPhepToan, tvKetQua;
     int a, b, ketQuaDung;
     String phepToan;
@@ -29,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.btn7);
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
+
+        tvSoA = findViewById(R.id.tvSoA);
+        tvSoB = findViewById(R.id.tvSoB);
+        tvPhepToan = findViewById(R.id.tvPhepToan);
+        tvKetQua = findViewById(R.id.tvKetQua);
+        btnKiemTra = findViewById(R.id.btnKiemTra);
 
 
         tvKetQua = findViewById(R.id.tvKetQua);
