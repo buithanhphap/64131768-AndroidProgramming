5b8635db3cbc78e90b9b9ca1cfd63d067107eb33
phapvuong <btphap.91@gmail.com>
2025-03-20 19:44:02 +0700

    update

--- KidsLearning/app/src/main/java/ntu/buithanhphap/kidslearning/HomeActivity.java
+++ KidsLearning/app/src/main/java/ntu/buithanhphap/kidslearning/HomeActivity.java
@@ -1,6 +1,8 @@
 package ntu.buithanhphap.kidslearning;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        View view8 = findViewById(R.id.view8);
+        view8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, PaletteActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
