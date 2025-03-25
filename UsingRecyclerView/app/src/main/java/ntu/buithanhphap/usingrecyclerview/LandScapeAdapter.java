4bf6e348abfb720efb01ddd414a98bc2b5352a58
phapvuong <btphap.91@gmail.com>
2025-03-25 20:52:35 +0700

    update code bên lớp

---
+++ UsingRecyclerView/app/src/main/java/ntu/buithanhphap/usingrecyclerview/LandScapeAdapter.java
@@ -0,0 +1,22 @@
+package ntu.buithanhphap.usingrecyclerview;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<> {
+    class ItemLandHolder extends RecyclerView.ViewHolder{
+        TextView tvCaption;
+        ImageView ivLandscape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCation);
+            ivLandscape = itemView.findViewById(R.id.imageViewLand);
+        }
+    }
+
+}
