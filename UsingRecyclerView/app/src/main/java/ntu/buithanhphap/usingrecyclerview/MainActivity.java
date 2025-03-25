16279a0a42a7ad8971396298e5d531a87b43dd4b
phapvuong <btphap.91@gmail.com>
2025-03-25 21:26:33 +0700

    da hoan thien

--- UsingRecyclerView/app/src/main/java/ntu/buithanhphap/usingrecyclerview/MainActivity.java
+++ UsingRecyclerView/app/src/main/java/ntu/buithanhphap/usingrecyclerview/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -19,11 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //3
+        recyclerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewLandscape = findViewById(R.id.recyclerland);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        //7
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
     }
     ArrayList<LandScape> getDataForRecyclerView() {
         ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
         LandScape landScape1 = new LandScape("flag_tower_of_hanoi","Cột cờ H Nội");
         dsDuLieu.add(landScape1);
+        dsDuLieu.add( new LandScape("effel", "Tháp Effel"));
+        dsDuLieu.add( new LandScape("buckingham", "Cung điện Buckingham"));
+        dsDuLieu.add( new LandScape("statue_of_liberty", "Tượng nữ thần tự do"));
+        return dsDuLieu;
     }
 }
