078cc2f74f700c278a166d1bf387b8963e768546
phapvuong <btphap.91@gmail.com>
2025-03-25 21:51:56 +0700

    update

--- UsingRecyclerView/app/src/main/java/ntu/buithanhphap/usingrecyclerview/LandScapeAdapter.java
+++ UsingRecyclerView/app/src/main/java/ntu/buithanhphap/usingrecyclerview/LandScapeAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -51,7 +52,7 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
         return LstData.size();
     }
 
-    class ItemLandHolder extends RecyclerView.ViewHolder{
+    class ItemLandHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tvCaption;
         ImageView ivLandscape;
 
@@ -59,6 +60,20 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
             super(itemView);
             tvCaption = itemView.findViewById(R.id.textViewCation);
             ivLandscape = itemView.findViewById(R.id.imageViewLand);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //Code ở đây
+            int viTriDuocClick = getAdapterPosition();
+            LandScape phanTuDuocClick = LstData.get(viTriDuocClick);
+            // bóc thông tin
+            String ten = phanTuDuocClick.getLandCation();
+            String tenFile = phanTuDuocClick.getLandImageFileName();
+            //Toast Tên
+            String chuoiThongBao = "Bạn vừa click vào: " + ten;
+            Toast.makeText(v.getContext(), chuoiThongBao, Toast.LENGTH_SHORT).show();
         }
     }
 }
