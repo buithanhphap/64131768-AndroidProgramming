1e17997e6aa1ed2d0eb4f3a2cee49d776385a43c
phapvuong <btphap.91@gmail.com>
2025-03-25 20:56:20 +0700

    Xong lớp LandScapeAdapter

--- UsingRecyclerView/app/src/main/java/ntu/buithanhphap/usingrecyclerview/LandScapeAdapter.java
+++ UsingRecyclerView/app/src/main/java/ntu/buithanhphap/usingrecyclerview/LandScapeAdapter.java
@@ -1,13 +1,30 @@
 package ntu.buithanhphap.usingrecyclerview;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LandScapeAdapter extends RecyclerView.Adapter<> {
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     class ItemLandHolder extends RecyclerView.ViewHolder{
         TextView tvCaption;
         ImageView ivLandscape;
