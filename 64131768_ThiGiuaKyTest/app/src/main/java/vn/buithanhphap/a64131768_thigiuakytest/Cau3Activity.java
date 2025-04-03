0df979eb9cf250fea48f0afe54dc0506c196475f
phapvuong <btphap.91@gmail.com>
2025-04-03 18:18:49 +0700

    xong

--- 64131768_ThiGiuaKyTest/app/src/main/java/vn/buithanhphap/a64131768_thigiuakytest/Cau3Activity.java
+++ 64131768_ThiGiuaKyTest/app/src/main/java/vn/buithanhphap/a64131768_thigiuakytest/Cau3Activity.java
@@ -1,6 +1,9 @@
 package vn.buithanhphap.a64131768_thigiuakytest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Cau3Activity extends AppCompatActivity {
+    Button btnLienHe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cau3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnLienHe = findViewById(R.id.btnLienHe);
+        btnLienHe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Cau3Activity.this, "Đã liên hệ thành công!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
--- 64131768_ThiGiuaKyTest/app/src/main/java/vn/buithanhphap/a64131768_thigiuakytest/Cau4Activity.java
+++ 64131768_ThiGiuaKyTest/app/src/main/java/vn/buithanhphap/a64131768_thigiuakytest/Cau4Activity.java
@@ -7,18 +7,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Cau4Activity extends AppCompatActivity {
-
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cau4);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //3
+        recyclerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        //7
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecyclerView() {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("mec","Mercedes-Benz C200 Avantgarde");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add( new LandScape("kia", "KIA Carens 1.4T Premium"));
+        dsDuLieu.add( new LandScape("honda", "Honda City RS"));
+        dsDuLieu.add( new LandScape("bwm", " BWM M5"));
+        return dsDuLieu;
     }
 }
