a931aa4a39831c23999de35533f0147045eb6356
phapvuong <btphap.91@gmail.com>
2025-04-04 15:30:40 +0700

    khai báo và tìm điều khiển

--- ThiGK2BuiThanhPhap/app/src/main/java/thigk2/buithanhphap/thigk2buithanhphap/MainActivity.java
+++ ThiGK2BuiThanhPhap/app/src/main/java/thigk2/buithanhphap/thigk2buithanhphap/MainActivity.java
@@ -1,6 +1,7 @@
 package thigk2.buithanhphap.thigk2buithanhphap;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +11,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1, btn2, btn3, btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        
     }
 }
