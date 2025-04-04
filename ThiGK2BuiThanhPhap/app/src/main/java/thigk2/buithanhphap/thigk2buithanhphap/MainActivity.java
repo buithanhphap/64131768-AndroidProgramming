d5d318ea78b3660ee1c36dc4ff0c6c7bfdbcff2d
phapvuong <btphap.91@gmail.com>
2025-04-04 15:33:48 +0700

    viết chuyển màn và tạo thêm

--- ThiGK2BuiThanhPhap/app/src/main/java/thigk2/buithanhphap/thigk2buithanhphap/MainActivity.java
+++ ThiGK2BuiThanhPhap/app/src/main/java/thigk2/buithanhphap/thigk2buithanhphap/MainActivity.java
@@ -1,6 +1,8 @@
 package thigk2.buithanhphap.thigk2buithanhphap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -20,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
         btn4 = findViewById(R.id.btn4);
-        
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Item1Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
