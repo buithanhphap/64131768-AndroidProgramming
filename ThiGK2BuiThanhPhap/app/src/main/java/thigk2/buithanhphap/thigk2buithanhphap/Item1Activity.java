26182e72fe90f0be95332a7516088712b8c3caa4
phapvuong <btphap.91@gmail.com>
2025-04-04 16:03:17 +0700

    cap nhat

--- ThiGK2BuiThanhPhap/app/src/main/java/thigk2/buithanhphap/thigk2buithanhphap/Item1Activity.java
+++ ThiGK2BuiThanhPhap/app/src/main/java/thigk2/buithanhphap/thigk2buithanhphap/Item1Activity.java
@@ -1,6 +1,9 @@
 package thigk2.buithanhphap.thigk2buithanhphap;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Item1Activity extends AppCompatActivity {
-
+    EditText edtNgay, edtThang, edtNam;
+    Button btnKiemTra;
+    TextView tvKetQua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
