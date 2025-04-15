a3bcde50c9d33642baea21d1d6b98dd383550365
phapvuong <btphap.91@gmail.com>
2025-04-15 07:12:10 +0700

    tạo hai fragment

--- FragmentEx_Replace/app/src/main/java/ntu/buithanhphap/fragmentex_replace/MainActivity.java
+++ FragmentEx_Replace/app/src/main/java/ntu/buithanhphap/fragmentex_replace/MainActivity.java
@@ -1,4 +1,4 @@
-package ntu.buithanhphap.fragmentex_replace;
+                                                       package ntu.buithanhphap.fragmentex_replace;
 
 import android.os.Bundle;
 
@@ -13,12 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
