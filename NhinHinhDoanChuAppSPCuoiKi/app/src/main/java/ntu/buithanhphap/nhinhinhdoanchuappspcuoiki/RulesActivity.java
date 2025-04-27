fdc70e39c3370cbd8a872fa657431c7566f4b1d3
phapvuong <btphap.91@gmail.com>
2025-04-27 16:35:28 +0700

    cập nhật và xử lý nút Quay Lai ben giao diện luat choi

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/RulesActivity.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/RulesActivity.java
@@ -1,6 +1,8 @@
 package ntu.buithanhphap.nhinhinhdoanchuappspcuoiki;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RulesActivity extends AppCompatActivity {
-
+    Button btnQuayLai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rules);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnQuayLai = findViewById(R.id.btnQuayLai);
+        btnQuayLai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
