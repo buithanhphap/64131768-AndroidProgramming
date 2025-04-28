bd8cf300748b824d849f52cd3e3c422afe3e8ba8
phapvuong <btphap.91@gmail.com>
2025-04-28 17:35:39 +0700

    tìm textview và gán chữ

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/DapAnAdapter.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/DapAnAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,7 @@ import java.util.List;
 public class DapAnAdapter extends ArrayAdapter<String> {
     Context context;
     ArrayList<String> arr;
+    TextView tvDapAn;
     public DapAnAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
         super(context, resource, objects);
         this.context = context;
@@ -28,6 +30,8 @@ public class DapAnAdapter extends ArrayAdapter<String> {
             LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.tra_loi, null);
         }
+        tvDapAn = convertView.findViewById(R.id.tvDapAn);
+        tvDapAn.setText(this.arr.get(position));
         return convertView;
     }
 }
