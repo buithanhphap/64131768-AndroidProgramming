6096dfabfabe0a2bdd4eaeb30c9d8291f98f2296
phapvuong <btphap.91@gmail.com>
2025-04-28 17:27:00 +0700

    up

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameActivity.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameActivity.java
@@ -20,11 +20,16 @@ public class PlayGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play_game);
         //Tim dieu khien
         gdvDapAn = findViewById(R.id.gdvDapAn);
+        // Khởi tạo dữ liệu
         ArrDapAn = new ArrayList<>();
         ArrDapAn.add("D");
         ArrDapAn.add("D");
         ArrDapAn.add("D");
         ArrDapAn.add("D");
         ArrDapAn.add("D");
+        // Set số cột bằng đúng số lượng phần tử => nằm ngang
+        gdvDapAn.setNumColumns(ArrDapAn.size());
+        // Gán adapter
+        gdvDapAn.setAdapter(new DapAnAdapter(this, 0, ArrDapAn));
     }
 }
