1abbd867241084b9349b152c0130dc229e576217
phapvuong <btphap.91@gmail.com>
2025-04-28 21:50:53 +0700

    xử lý cho nhập đáp án tương tự hiện đáp án

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameActivity.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameActivity.java
@@ -22,6 +22,7 @@ public class PlayGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play_game);
         //Tim dieu khien
         gdvDapAn = findViewById(R.id.gdvDapAn);
+        gdvNhapDapAn = findViewById(R.id.gdvNhapDapAn);
         // Khởi tạo dữ liệu
         ArrDapAn = new ArrayList<>();
         ArrDapAn.add("D");
@@ -29,9 +30,22 @@ public class PlayGameActivity extends AppCompatActivity {
         ArrDapAn.add("D");
         ArrDapAn.add("D");
         ArrDapAn.add("D");
+
+        ArrNhapDapAn = new ArrayList<>();
+        ArrNhapDapAn.add("A");
+        ArrNhapDapAn.add("B");
+        ArrNhapDapAn.add("C");
+        ArrNhapDapAn.add("D");
+        ArrNhapDapAn.add("E");
+        ArrNhapDapAn.add("A");
+        ArrNhapDapAn.add("O");
         // Set số cột bằng đúng số lượng phần tử => nằm ngang
         gdvDapAn.setNumColumns(ArrDapAn.size());
         // Gán adapter
         gdvDapAn.setAdapter(new DapAnAdapter(this, 0, ArrDapAn));
+        // Set số cột bằng đúng số lượng phần tử => nằm ngang
+        gdvNhapDapAn.setNumColumns(ArrNhapDapAn.size()/2);
+        // Gán adapter
+        gdvNhapDapAn.setAdapter(new DapAnAdapter(this, 0, ArrNhapDapAn));
     }
 }
