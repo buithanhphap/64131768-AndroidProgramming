1a0f78b6850f3f02c9af9fd96321ae54bd55fc40
phapvuong <btphap.91@gmail.com>
2025-04-29 22:59:11 +0700

    up

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameActivity.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -22,6 +25,7 @@ public class PlayGameActivity extends AppCompatActivity {
      private String dapAn="CADAO";
      int index = 0;
     ArrayList<Integer> ViTriBanDau;
+    ImageView imgHinhCauHoi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,7 @@ public class PlayGameActivity extends AppCompatActivity {
         //Tim dieu khien
         gdvDapAn = findViewById(R.id.gdvDapAn);
         gdvNhapDapAn = findViewById(R.id.gdvNhapDapAn);
+        imgHinhCauHoi = findViewById(R.id.imgHinhCauHoi);
         OnClick();
         // Khởi tạo dữ liệu
         models = new PlayGameModel(this);
@@ -138,5 +143,8 @@ public class PlayGameActivity extends AppCompatActivity {
         gdvNhapDapAn.setNumColumns(ArrNhapDapAn.size()/2);
         // Gán adapter
         gdvNhapDapAn.setAdapter(new DapAnAdapter(this, 0, ArrNhapDapAn));
+        Glide.with(this)
+                .load(hinhCauHoi.hinhAnh)
+                .into(imgHinhCauHoi);
     }
 }
--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameModel.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameModel.java
@@ -6,19 +6,26 @@ public class PlayGameModel {
     PlayGameActivity p;
     ArrayList<HinhCauHoi> arr;
     int cauSo = 0;
+
     public PlayGameModel(PlayGameActivity p) {
         this.p = p;
         khoiTaoDaTa();
     }
+
     private void khoiTaoDaTa() {
         arr = new ArrayList<>();
-        arr.add(new HinhCauHoi("Màn 1","baocao","https://freetuts.net/upload/product_series/images/2022/11/16/2598/nhin-hinh-doan-chu-2.jpg"));
+        arr.add(new HinhCauHoi("Màn 1", "yeuot", "https://freetuts.net/upload/product_series/images/2022/11/16/2598/nhin-hinh-doan-chu-60.jpg"));
+        arr.add(new HinhCauHoi("Màn 2", "baocao", "https://freetuts.net/upload/product_series/images/2022/11/16/2598/nhin-hinh-doan-chu-2.jpg"));
     }
-    public HinhCauHoi LayHinhCauHoi(){
-        cauSo++;
-        if(cauSo>= arr.size()){
-            cauSo= arr.size()-1;
+
+    public HinhCauHoi LayHinhCauHoi() {
+        if (cauSo >= arr.size()) {
+            cauSo = arr.size() - 1; 
         }
         return arr.get(cauSo);
+    }
+
+    public void NextCauHoi() {
+        cauSo++;
     }
 }
