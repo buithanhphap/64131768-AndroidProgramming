0ab37be30bb65ce2ed1e0d1208e415f128001f09
phapvuong <btphap.91@gmail.com>
2025-04-29 21:59:18 +0700

    khởi tạo data

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameModel.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameModel.java
@@ -1,5 +1,15 @@
 package ntu.buithanhphap.nhinhinhdoanchuappspcuoiki;
 
+import java.util.ArrayList;
+
 public class PlayGameModel {
-    
+    PlayGameActivity p;
+    ArrayList<HinhCauHoi> arr;
+    int cauSo = 0;
+    public PlayGameModel(PlayGameActivity p) {
+        this.p = p;
+    }
+    private void khoiTaoDaTa() {
+        
+    }
 }
