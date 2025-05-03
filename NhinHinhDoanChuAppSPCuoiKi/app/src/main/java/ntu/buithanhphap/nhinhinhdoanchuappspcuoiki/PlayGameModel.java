1d4db9dbb788a6cf6d53e07ae75fa7159012b163
buithanhphap <166505570+buithanhphap@users.noreply.github.com>
2025-05-03 17:35:00 +0700

    up thêm dữ liệu câu hỏi

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameModel.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/PlayGameModel.java
@@ -19,6 +19,13 @@ public class PlayGameModel {
         arr.add(new HinhCauHoi("Màn 1", "yeuot", "https://freetuts.net/upload/product_series/images/2022/11/16/2598/nhin-hinh-doan-chu-60.jpg"));
         arr.add(new HinhCauHoi("Màn 2", "baocao", "https://freetuts.net/upload/product_series/images/2022/11/16/2598/nhin-hinh-doan-chu-2.jpg"));
         arr.add(new HinhCauHoi("Màn 3", "cadao", "https://i.ytimg.com/vi/rHtDfzSPZfc/maxresdefault.jpg"));
+        arr.add(new HinhCauHoi("Màn 4", "mitom", "https://i.ytimg.com/vi/p3RQvawhPvA/sddefault.jpg"));
+        arr.add(new HinhCauHoi("Màn 5", "phuyen", "https://cdn2.tuoitre.vn/thumb_w/480/471584752817336320/2023/4/20/phu-yen-1-16819671427591853833321.jpg"));
+        arr.add(new HinhCauHoi("Màn 6", "taomeo", "https://i.ytimg.com/vi/7UeuqlU4gbE/maxresdefault.jpg"));
+        arr.add(new HinhCauHoi("Màn 7", "camap", "https://i.ytimg.com/vi/gZqv6D38bc0/maxresdefault.jpg"));
+        arr.add(new HinhCauHoi("Màn 8", "nhama", "https://i.ytimg.com/vi/TgPkpguoi4g/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBkoMVkfEh5SeAJKp3EzXuVkPrezA"));
+        arr.add(new HinhCauHoi("Màn 9", "halong", "https://i.ytimg.com/vi/xRxrUtzBiv8/maxresdefault.jpg"));
+        arr.add(new HinhCauHoi("Màn 10", "nauan", "https://i.ytimg.com/vi/r_U4PxrY7bw/sddefault.jpg"));
     }
 
     public HinhCauHoi LayHinhCauHoi() {
