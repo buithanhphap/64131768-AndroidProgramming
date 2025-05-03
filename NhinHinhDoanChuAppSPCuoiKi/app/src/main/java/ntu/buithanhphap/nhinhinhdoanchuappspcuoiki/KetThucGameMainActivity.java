8d411b4074dbd07b69887f53a1a3f17ed07519a9
buithanhphap <166505570+buithanhphap@users.noreply.github.com>
2025-05-03 21:13:42 +0700

    khai bao va tim dieu khien

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/KetThucGameMainActivity.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/KetThucGameMainActivity.java
@@ -1,6 +1,7 @@
 package ntu.buithanhphap.nhinhinhdoanchuappspcuoiki;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class KetThucGameMainActivity extends AppCompatActivity {
-
+    Button btnChoiLai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ket_thuc_game_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btnChoiLai = findViewById(R.id.btnChoiLai);
+        
     }
 }
