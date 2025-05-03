b366c291140c8b58fd356ed557d22b126f5140a5
buithanhphap <166505570+buithanhphap@users.noreply.github.com>
2025-05-03 21:14:47 +0700

    xử lý khi nhấn chơi lại qua màn hình giao diện play game

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/KetThucGameMainActivity.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/KetThucGameMainActivity.java
@@ -1,6 +1,8 @@
 package ntu.buithanhphap.nhinhinhdoanchuappspcuoiki;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,13 @@ public class KetThucGameMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ket_thuc_game_main);
         btnChoiLai = findViewById(R.id.btnChoiLai);
-        
+        btnChoiLai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KetThucGameMainActivity.this, PlayGameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
