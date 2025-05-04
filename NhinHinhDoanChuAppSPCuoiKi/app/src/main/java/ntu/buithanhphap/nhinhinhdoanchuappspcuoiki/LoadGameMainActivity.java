dee09e01e260e69e3e8b6af3a1a46ce66f118ab5
phapvuong <btphap.91@gmail.com>
2025-05-04 19:58:55 +0700

    xử lý loanding

--- NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/LoadGameMainActivity.java
+++ NhinHinhDoanChuAppSPCuoiKi/app/src/main/java/ntu/buithanhphap/nhinhinhdoanchuappspcuoiki/LoadGameMainActivity.java
@@ -1,6 +1,9 @@
 package ntu.buithanhphap.nhinhinhdoanchuappspcuoiki;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,14 @@ public class LoadGameMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_load_game_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadGameMainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // Độ trễ 3000ms (3 giây)
     }
 }
